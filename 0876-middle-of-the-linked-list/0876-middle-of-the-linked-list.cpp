2cfbfa5c594fc43ccdbbd2cf5566d462610987aa
Ritika Verma <124219354+ansi22@users.noreply.github.com>
2026-08-04 00:37:13 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.88%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -10,14 +10,15 @@ struct ListNode {
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
+       ListNode* slow=head;
+       ListNode* fast=head;
 
-        while(fast!=NULL && fast->next!=NULL) {
-            slow=slow->next;
-            fast=fast->next->next;
-        }
+       while(fast!=NULL and fast->next!=NULL) {
+        slow=slow->next;
+        fast=fast->next->next;
+       }
 
-        return slow;
+       return slow;
     }
 };
+
