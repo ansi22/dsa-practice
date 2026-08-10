f92135989214e6866a515a1bd888c94bfb1bfafb
Ritika Verma <124219354+ansi22@users.noreply.github.com>
2026-08-10 18:45:19 +0530

    Time: 0 ms (100%), Space: 19.9 MB (85.1%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size(), ans=0;
+
+        for(int i=1;i<n;i++) {
+            ans+=max(prices[i]-prices[i-1], 0);
+        }
+
+        return ans;
+    }
+};
