0db8e240419093b0019b16fa3035c5b130f875b4
Ritika Verma <124219354+ansi22@users.noreply.github.com>
2026-08-20 23:43:26 +0530

    Time: 41 ms (3.72%), Space: 11 MB (6.47%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-
     int memo[1001][51];
     int getMinimumLargestSplitSum(vector<int>& prefixSum, int cur, int subCnt) {
         int n=prefixSum.size()-1;
+
         if(memo[cur][subCnt]!=-1) {
             return memo[cur][subCnt];
         }
+
         if(subCnt==1) {
             return memo[cur][subCnt]=prefixSum[n]-prefixSum[cur];
         }
-        int minimumLargestSplitSum = INT_MAX;
+
+        int minlsum=INT_MAX;
         for(int i=cur;i<=n-subCnt;i++) {
             int firstSplitSum=prefixSum[i+1]-prefixSum[cur];
             int largestSplitSum=max(firstSplitSum, getMinimumLargestSplitSum(prefixSum, i+1, subCnt-1));
-            minimumLargestSplitSum=min(minimumLargestSplitSum, largestSplitSum);
-            if(firstSplitSum>=minimumLargestSplitSum) {
+            minlsum=min(minlsum, largestSplitSum);
+
+            if(firstSplitSum>=minlsum) {
                 break;
             }
         }
-        return memo[cur][subCnt]=minimumLargestSplitSum;
+
+        return memo[cur][subCnt]=minlsum;
     }
     int splitArray(vector<int>& nums, int k) {
         memset(memo, -1, sizeof(memo));
-
         int n=nums.size();
         vector<int> prefixSum(n+1, 0);
+
         for(int i=0;i<n;i++) {
             prefixSum[i+1]=prefixSum[i]+nums[i];
         }
@@ -33,8 +37,3 @@ public:
         return getMinimumLargestSplitSum(prefixSum, 0, k);
     }
 };
-/*
-options: i. to take in current array
-         ii. to start a new array
-
-*/ 
