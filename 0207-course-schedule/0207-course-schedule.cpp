0607b31bb61a9cfd2f832a4794ddaeb4ad25ac1e
Ritika Verma <124219354+ansi22@users.noreply.github.com>
2026-09-13 23:39:24 +0530

    Time: 18 ms (7.55%), Space: 20.1 MB (25.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
-        if(pre.empty()) {
+        if(pre.empty()){
             return 1;
         }
 
         priority_queue<int, vector<int>, greater<int>> nodeg;
         vector<vector<int>> graph(n+1);
         vector<int> indegree(n+1);
+
         int check=0;
-        
+
         for(auto x: pre) {
             graph[x[0]].push_back(x[1]);
             indegree[x[1]]++;
@@ -20,11 +21,13 @@ public:
                 nodeg.push(i);
             }
         }
+        int ans=0;
 
         while(!nodeg.empty()) {
             auto node=nodeg.top();
             nodeg.pop();
-            check++;
+            ans++;
+
 
             for(auto child: graph[node]) {
                 indegree[child]--;
@@ -32,18 +35,8 @@ public:
                     nodeg.push(child);
                 }
             }
-
         }
 
-        return check==n;
+        return ans==n;
     }
 };
-
-/*
-
-Uses in-degree counting.
-Repeatedly removes nodes with zero in-degree, adds them to the result, and reduces in-degree of their neighbors.
-If not all nodes are processed, a cycle exists.
-Time complexity: $ O(V + E) $
-
-*/ 
